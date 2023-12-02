eaa8e29794b504191754333704dc96d10a5339a2
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-02 19:32:34 +0100

    method for checking if number is divisible by two

---
+++ src/main/java/Numbers.java
@@ -0,0 +1,8 @@
+public class Numbers {
+    public void checkIfNumbersIsDivisibleByTwo(int number){
+        if (number % 2 == 0)
+            System.out.println("This number can be divided by two!");
+        else
+            System.out.println("This number is not divisible by two!");
+    }
+}
