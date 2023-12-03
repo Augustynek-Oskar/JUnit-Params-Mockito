8683dabcdabcf88c96daea19d8546186c243180e
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-03 20:39:59 +0100

    working getUppercase method with unit test for null and empty exceptions

--- src/main/java/UpperCase.java
+++ src/main/java/UpperCase.java
@@ -1,17 +1,10 @@
 public class UpperCase {
+    public static void main(String[] args) {
+        getUpperCase("");
+    }
     public static String getUpperCase (String text){
-        try {
-            if (text == null) return "text";
-            if (text == "") throw new RuntimeException();
-
-        } catch (NullPointerException e){
-            System.out.println(e.getMessage());
-            return "";
-
-        } catch (RuntimeException e){
-            System.out.println("Argument cannot be empty!");
-            return "";
-        }
-        return text.toUpperCase();
+            if (text == null) throw new NullPointerException("Text cannot be null!");
+            else if (text == "") throw new RuntimeException("Text cannot be empty!");
+            else return text.toUpperCase();
     }
 }
--- src/test/java/UpperCaseTest.java
+++ src/test/java/UpperCaseTest.java
@@ -7,11 +7,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UpperCaseTest {
     @ParameterizedTest
-    @NullAndEmptySource
-    @ValueSource (strings = {"Witam", "aBcD", "aaaa"})
+    @ValueSource (strings = {"Witam123", "aBcD", "aaaa"})
     void shouldGetUpperCase(String input) {
-        UpperCase upperCase = new UpperCase();
-        String expectedText = UpperCase.getUpperCase(input);
-        Assertions.assertEquals(shouldGetUpperCase(input), expectedText);
+        String actualText = UpperCase.getUpperCase(input);
+        Assertions.assertEquals(input.toUpperCase(), actualText);
+    }
+    @ParameterizedTest
+    @NullSource
+    void shouldThrowNullPointerException(String input) {
+        Assertions.assertThrows(NullPointerException.class, () -> UpperCase.getUpperCase(null) );
+    }
+    @ParameterizedTest
+    @EmptySource
+    void shouldThrowRuntimeException(String input) {
+        Assertions.assertThrows(RuntimeException.class, () -> UpperCase.getUpperCase("") );
     }
 }
