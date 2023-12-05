d1ab67b36107df978a7d4ce131b5b3d5ba9aa46e
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-05 11:22:35 +0100

    Mockito unit tests for calculator

--- src/main/java/CurrencyCalculator.java
+++ src/main/java/CurrencyCalculator.java
@@ -1,16 +1,19 @@
 public class CurrencyCalculator implements CurrencyExchangeRate{
 
-    public static float plnToEuroCalculator (float amountInZlotys){
+    public float plnToEuroCalculator (float amountInZlotys){
+        if (amountInZlotys == 0) return 0;
         float convertedAmount = amountInZlotys / CurrencyExchangeRate.euroToPlnBuyRate();
         System.out.println(amountInZlotys + " " + Currency.PLN + " is worth: " + convertedAmount + " " + Currency.EURO);
         return convertedAmount;
     }
-    public static float plnToUsdCalculator (float amountInZlotys){
+    public float plnToUsdCalculator (float amountInZlotys){
+        if (amountInZlotys == 0) return 0;
         float convertedAmount = amountInZlotys / CurrencyExchangeRate.usdToPlnBuyRate();
         System.out.println(amountInZlotys + " " + Currency.PLN + " is worth: " + convertedAmount + " " + Currency.USD);
         return convertedAmount;
     }
-    public static float plnToGbpCalculator (float amountInZlotys){
+    public float plnToGbpCalculator (float amountInZlotys){
+        if (amountInZlotys == 0) return 0;
         float convertedAmount = amountInZlotys / CurrencyExchangeRate.gbpToPlnBuyRate();
         System.out.println(amountInZlotys + " " + Currency.PLN + " is worth: " + convertedAmount + " " + Currency.GBP);
         return convertedAmount;
--- src/main/java/CurrencyExchangeRate.java
+++ src/main/java/CurrencyExchangeRate.java
@@ -1,21 +1,9 @@
 public interface CurrencyExchangeRate {
 
-    static float euroToPlnBuyRate (){
-        return 4.33f;
-    }
-    static float euroToPlnSellRate (){
-        return 4.15f;
-    }
-    static float usdToPlnBuyRate(){
-        return 4.00f;
-    }
-    static float usdToPlnSellRate(){
-        return 3.88f;
-    }
-    static float gbpToPlnBuyRate(){
-        return 5.05f;
-    }
-    static float gbpToPlnSellRate(){
-        return 4.95f;
-    }
+    static float euroToPlnBuyRate () {return 4.33f;}
+    static float euroToPlnSellRate () {return 4.15f;}
+    static float usdToPlnBuyRate() {return 4.00f;}
+    static float usdToPlnSellRate() {return 3.88f;}
+    static float gbpToPlnBuyRate() {return 5.05f;}
+    static float gbpToPlnSellRate() {return 4.95f;}
 }
