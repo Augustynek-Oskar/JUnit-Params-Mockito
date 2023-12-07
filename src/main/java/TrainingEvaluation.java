5a2a9e994614d53e97f101ef58d74e729c1277f1
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-07 19:54:07 +0100

    update

--- src/main/java/TrainingEvaluation.java
+++ src/main/java/TrainingEvaluation.java
@@ -1,3 +1,30 @@
-public class TrainingEvaluation {
-    int rating;
+public class TrainingEvaluation{
+    public static void main(String[] args) {
+        TrainingWatch trainingWatch = new TrainingWatch();
+        float overallRating = (float) (lengthRating(trainingWatch.getTrainingLength()) + burnedKcalRating(trainingWatch.getCaloriesBurned()) + avgBpmRating(trainingWatch.getAveragePulse())) / 6;
+        System.out.println("Your rating is: " + overallRating);
+
+
+    }
+    public static int lengthRating(int length){
+        if (length < 30) return 1;
+        if (length >= 30 && length <= 60) return 2;
+        if (length > 60) return 3;
+        else return 0;
+    }
+
+    public static int burnedKcalRating(int burnedCalories){
+        if (burnedCalories <= 300) return 1 * 2;
+        if (burnedCalories > 300 && burnedCalories < 400) return 2 * 2;
+        if (burnedCalories >= 400) return 3 * 2;
+        else return 0;
+    }
+
+    public static int avgBpmRating(int bpm){
+        if (bpm > 175) return 1 * 3;
+        if (bpm >= 160 && bpm <= 175) return 2 * 3;
+        if (bpm < 160) return 3 * 3;
+        else return 0;
+    }
+
 }
--- src/main/java/TrainingWatch.java
+++ src/main/java/TrainingWatch.java
@@ -1,7 +1,7 @@
-public class TrainingWatch {
-    private int trainingLength;
-    private int caloriesBurned;
-    private int averagePulse;
+public class TrainingWatch extends TrainingEvaluation{
+    private int trainingLength = 45;
+    private int caloriesBurned = 250;
+    private int averagePulse = 175;
 
     public int getTrainingLength() {
         return trainingLength;
