c230be9796441ca92eced50a8d0479001cfdbf4a
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-09 19:26:10 +0100

    Added class Employee with personal info Interface for base salary Interface for saturday bonus

---
+++ src/main/java/Employee.java
@@ -0,0 +1,20 @@
+public class Employee {
+    int hoursWorked;
+    int payForHour;
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public int getPayForHour() {
+        return payForHour;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public void setPayForHour(int payForHour) {
+        this.payForHour = payForHour;
+    }
+}
--- src/main/java/SalaryCalculator.java
+++ src/main/java/SalaryCalculator.java
@@ -1,2 +1,9 @@
-public class SalaryCalculator {
+public class SalaryCalculator extends Employee implements BaseSalary, SaturdayBonus {
+
+    @Override
+    public int getBaseSalary(int hoursWorked) {
+        Employee employee = new Employee();
+        employee.hoursWorked = hoursWorked;
+        return employee.hoursWorked * employee.payForHour;
+    }
 }
